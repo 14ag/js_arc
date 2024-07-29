e2fff6d5777d35fb7333843f7080594ff88cf20b
14ag <muriukipn@gmail.com>
2024-07-30 02:54:07 +0300

    30/7

--- learning_functions.js
+++ learning_functions.js
@@ -1,11 +1,25 @@
+//import println using es6
 import { println } from "./utils.js";
-function outer(){
-    let foo=0;
-    return function inner(){
-        foo++;
-        return foo;
-    };
-}
-const blt=outer();
-println(blt());
-println(blt());
+
+// // function showing closures in js
+// function outer(){
+//     let foo=0;
+//     return function inner(){
+//         foo++;
+//         return foo;
+//     };
+// }
+// const blt=outer();
+// println(blt());
+// println(foo)
+// println(blt());
+//==========================================================================================================
+
+// //arrow functions. 7 is the default value
+// const arrow=(x=7)=>x+2;
+// println(arrow(8))
+
+//==========================================================================================================
+let numbes=[1,2,3,4,5];
+numbes.push(thisItem)
+println(numbes)
