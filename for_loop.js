bbdde0eba71b9abfbe661ec76cc05f780e0a0795
14ag <muriukipn@gmail.com>
2024-07-30 02:54:34 +0300

    30/7

---
+++ for_loop.js
@@ -0,0 +1,10 @@
+import { println } from "./utils";
+
+// finding out how for loop works exactly
+for (let i=0; i<6; i++) {
+    println("x");
+    println(i +"th time");
+    println("")
+    println("")
+    // console.log(i + "th time")
+}
