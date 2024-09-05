14e3befdb63d46ed01971ba5782c3f85dc90c18f
14ag <muriukipn@gmail.com>
2024-09-05 18:24:46 +0300

    5/9

--- testbox/formatted_table.js
+++ testbox/formatted_table.js
@@ -1,4 +1,5 @@
-import { println,row_entry,to_binary } from "./utils_v2";
+import  println from "../utils";
+import row_entry from '../utils_v2';
 
 // const words = "apple", "banana", "cherry", "date";
 println(row_entry("-","apple", "banana", "cherry", "date"));
