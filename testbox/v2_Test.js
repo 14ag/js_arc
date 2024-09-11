20ec56c82cf43005b1b5f82c812fbe73c71135df
14ag <muriukipn@gmail.com>
2024-09-11 21:14:21 +0300

    11/9

---
+++ testbox/v2_Test.js
@@ -0,0 +1,4 @@
+import { row_entry } from "../utils_v2";
+import { println } from "../utils";
+// let words = "apple", "banana", "cherry", "date";
+println(row_entry(1, "apple", "banana", "cherry", "date"));
--- utils_v2.js
+++ utils_v2.js
@@ -3,8 +3,13 @@ export function row_entry(s_pace=" ",...strings) {
     if (strings.length === 0) {
         return "noll"; // Or handle empty array differently
     }
-    var longest_string=strings.reduce((istring,nstring)=>istring.length>nstring.length?istring:nstring,'');   
-    
+    // var longest_string=strings.reduce((istring,nstring)=>istring.length>nstring.length?istring:nstring,'');   
+    let longest_string = arrays.forEach(item => {
+        // Check if the item is a string and if it's longer than the current longest
+        if (typeof item === 'string' && item.length > longest.length) {
+            longest = item;
+            }
+            });
     let place_holder=s_pace.repeat(longest_string.length);
     // strings=strings.toString()
     console.log(strings.toString());
@@ -14,16 +19,40 @@ export function row_entry(s_pace=" ",...strings) {
     console.log(strings)
     // let entry="";
 
-    let x=strings
     for (let string of strings){
         // console.log(string)
         // entry=en" "+string)
         entry=entry+" "+place_holder.slice(0,place_holder.length-string.length)+string;
     }
        let entrys=entry.join();
-       console.log("l  "+entrys)
-    //    return entrys;
+       console.log("  "+entrys)
+       return entrys;
 
         
     // }
 }
+
+
+
+/**
+
+
+s_pace=s_pace.toString()
+if (strings.length === 0) {
+    return "noll"; // Or handle empty array differently
+}
+var longest_string=strings.reduce((istring,nstring)=>istring.length>nstring.length?istring:nstring,'');   
+
+let rowEntry =(a)=>{
+    let cells=(b)=>{
+        //placeholder= char* (longest_string_length - length_of_string_passed)
+        //cellEntry= placeholder+ string_passed)
+    }
+    //loop that creates cellentries then concats them in a var
+    // return the var
+} 
+return rowEntry;
+// }
+
+
+**/
